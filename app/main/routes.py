016d624233e3a0a226ac66d15af355f8f202011c
Og B. Maciel <omaciel@ogmaciel.com>
2016-05-18 17:29:44 -0400

    Sort TestResults by date, then by operatingsystem major version.

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,11 @@
 from flask import render_template, redirect, url_for, request
-from ..models import TestRun
+from ..models import OperatingSystem, TestRun
 from . import main
 
 
 @main.route('/')
 def index():
-    test_runs = TestRun.query.all()
+    test_runs = TestRun.query.join(
+        OperatingSystem).filter().order_by(
+            TestRun.timestamp.desc(), OperatingSystem.major_version.desc())
     return render_template('index.html', runs=test_runs)
