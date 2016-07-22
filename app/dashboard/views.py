e05e6d67322d46f4429de764d2ce1ef96b8a9dab
Og B. Maciel <omaciel@ogmaciel.com>
2016-07-21 21:23:03 -0400

    Added new averaged data chart to dashboard.
    Display averaged data results in the dashboard and shift tabular data to the
    bottom.

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,6 +1,8 @@
-from app.models import OperatingSystem, Release, TestRun
+from app.db import db
+from app.models import OperatingSystem, TestRun
 from flask import render_template, request
 from flask import Blueprint
+from sqlalchemy.sql.functions import func
 
 import json
 
@@ -27,8 +29,28 @@ def index():
 
     rows.reverse()
 
+    # Average numbers
+    avg_rows = db.session.query(
+        TestRun.name,
+        func.avg(TestRun.passed).label('passed'),
+        func.avg(TestRun.failed).label('failed'),
+        func.avg(TestRun.skipped).label('skipped'),
+    ).group_by(
+        TestRun.name).order_by(
+            TestRun.timestamp.desc(),
+            TestRun.name.desc(),
+        ).limit(items)
+    avg_data = []
+    for row in avg_rows:
+        avg_data.append([row.name, row.passed, row.failed, row.skipped])
+    avg_data.reverse()
+
     return render_template(
-        'index.html', data=json.dumps(rows), test_runs=test_runs)
+        'index.html',
+        avg_data=json.dumps(avg_data),
+        data=json.dumps(rows),
+        test_runs=test_runs,
+    )
 
 
 @dashboard_blueprint.app_errorhandler(404)
